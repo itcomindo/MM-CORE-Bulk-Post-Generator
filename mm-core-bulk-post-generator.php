c99eb747e9a8d21a4ebebb3af9cadb0dcc17dc82
Budi Haryono <mail.budiharyono@gmail.com>
2025-07-26 20:19:36 +0700

    starting

---
+++ mm-core-bulk-post-generator.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Plugin Name:       MM CORE Bulk Post Generator
+ * Plugin URI:        https://budiharyono.id/
+ * Description:       A simple plugin to generate bulk posts with spintax, local SEO, shortcodes, and schema markup.
+ * Version:           1.0.0
+ * Author:            Budi Haryono
+ * Author URI:        https://budiharyono.id/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       mm-core-bulk-post-generator
+ * Domain Path:       /languages
+ * Requires at least: 5.0
+ * Requires PHP:      7.0
+ */
+defined('ABSPATH') || die('No script kiddies please!');
